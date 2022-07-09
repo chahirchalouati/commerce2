e71e3d008f34a7cea20f017a409364cf95205b72
Chahir Chalouati <chehhhir@gmail.com>
2022-07-09 07:16:35 +0200

    inor change

--- src/main/java/com/code/dreamer/commerce2/Commerce2Application.java
+++ src/main/java/com/code/dreamer/commerce2/Commerce2Application.java
@@ -2,10 +2,13 @@ package com.code.dreamer.commerce2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableMongoAuditing
+@ConfigurationPropertiesScan
 public class Commerce2Application {
 
     public static void main(String[] args) {
