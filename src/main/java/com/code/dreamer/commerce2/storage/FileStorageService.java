f91b5925fdd292aba05d0b2c3e85f2a15f378cb3
Chahir Chalouati <chehhhir@gmail.com>
2022-07-09 07:15:19 +0200

    add filestorage

--- src/main/java/com/code/dreamer/commerce2/storage/FileStorageService.java
+++ src/main/java/com/code/dreamer/commerce2/storage/FileStorageService.java
@@ -1,2 +1,7 @@
-package com.code.dreamer.commerce2.storage;public class FileStorageService {
+package com.code.dreamer.commerce2.storage;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileStorageService {
+    String saveTempFile(MultipartFile file);
 }
--- src/main/java/com/code/dreamer/commerce2/storage/FileStorageServiceImpl.java
+++ src/main/java/com/code/dreamer/commerce2/storage/FileStorageServiceImpl.java
@@ -1,2 +1,14 @@
-package com.code.dreamer.commerce2.storage;public class FileStorageServiceImpl {
+package com.code.dreamer.commerce2.storage;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+@Service
+public class FileStorageServiceImpl implements FileStorageService {
+    @Override
+    public String saveTempFile(MultipartFile file) {
+        return UUID.randomUUID().toString();
+    }
 }
